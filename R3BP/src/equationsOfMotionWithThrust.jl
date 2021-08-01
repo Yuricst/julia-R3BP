5aaf05e0e3dbeb7f1978c38f1b89d7c589cf4a25
Yuricst <yuri.shimane@gmail.com>
2021-08-01 19:35:17 +0900

    EoM with control-law as option

--- R3BP/src/equationsOfMotionWithThrust.jl
+++ R3BP/src/equationsOfMotionWithThrust.jl
@@ -16,23 +16,23 @@ Right-hand side expression for state-vector in PCR3BP with thrust in m1 directio
 """
 function rhs_pcr3bp_thrust_m1dir!(du,u,p,t)
     # unpack parameters
-    mu, τ, mdot, tmax = p
+    μ, τ, mdot, tmax = p
     # unpack state
     x, y = u[1], u[2]
     vx, vy = u[3], u[4]
     # compute distances
-    r1 = sqrt( (x+mu)^2 + y^2 );
-    r2 = sqrt( (x-1+mu)^2 + y^2 );
+    r1 = sqrt( (x+μ)^2 + y^2 );
+    r2 = sqrt( (x-1+μ)^2 + y^2 );
 
     # compute thrust in m1 direction
-    thrust_vec = τ*tmax * [x+mu, y] / r1
+    thrust_vec = τ*tmax * [x+μ, y] / r1
 
     # derivatives of positions
     du[1] = u[3]
     du[2] = u[4]
     # derivatives of velocities
-    du[3] =  2*vy + x - ((1-mu)/r1^3)*(mu+x) + (mu/r2^3)*(1-mu-x) + thrust_vec[1]/u[5]
-    du[4] = -2*vx + y - ((1-mu)/r1^3)*y      - (mu/r2^3)*y        + thrust_vec[2]/u[5]
+    du[3] =  2*vy + x - ((1-μ)/r1^3)*(μ+x) + (μ/r2^3)*(1-μ-x) + thrust_vec[1]/u[5]
+    du[4] = -2*vx + y - ((1-μ)/r1^3)*y      - (μ/r2^3)*y        + thrust_vec[2]/u[5]
     # mass derivative
     du[5] = -mdot*abs(τ)
 end
@@ -133,6 +133,140 @@ function rhs_bcr4bp_sunenhanced!(du,u,p,t)
     du[5] = -2*vx + y - ((1-μ)/r1^3)*y      - (μ/r2^3)*y        + sun_v[2] + thrust_vec[2]/mass
     du[6] =           - ((1-μ)/r1^3)*z      - (μ/r2^3)*z        + sun_v[3] + thrust_vec[3]/mass
 
+    # mass derivative
+    du[7] = -mdot*τ
+end
+
+
+# --------------------------------------------------------------------------- #
+# EoM with control-law given as callable
+
+"""
+    rhs_pcr3bp_control!(du,u,p,t)
+
+Right-hand side expression for state-vector in PCR3BP with control.
+Expect control-law function in the form:
+
+    ```julia
+        tx, ty = control_law(du,u,p,t)
+    ```
+
+# Arguments
+    - `du`: cache array of duative of state-vector
+    - `u`: state-vector
+    - `p`: parameters, where p[1] = μ
+    - `t`: time
+"""
+function rhs_pcr3bp_control!(du,u,p,t)
+    # unpack parameters
+    μ, mdot, control_law, τ = p[1:4]
+    # get thrust direction
+    tx, ty = control_law(du,u,p,t)
+    # unpack state
+    x, y = u[1], u[2]
+    vx, vy = u[3], u[4]
+    m = u[5]
+    # compute distances
+    r1 = sqrt( (x+μ)^2 + y^2 );
+    r2 = sqrt( (x-1+μ)^2 + y^2 );
+    # derivatives of positions
+    du[1] = u[3]
+    du[2] = u[4]
+    # derivatives of velocities
+    du[3] = 2*vy + x - ((1-μ)/r1^3)*(μ+x) + (μ/r2^3)*(1-μ-x) + tx/m
+    du[4] = -2*vx + y - ((1-μ)/r1^3)*y - (μ/r2^3)*y + ty/m
+    # mass derivative
+    du[5] = -mdot*τ
+end
+
+
+
+"""
+    rhs_cr3bp_control!(du,u,p,t)
+
+Right-hand side expression for state-vector in CR3BP with control.
+Expect control-law function in the form:
+
+    ```julia
+        tx, ty, tz = control_law(du,u,p,t)
+    ```
+
+# Arguments
+    - `du`: cache array of duative of state-vector
+    - `u`: state-vector
+    - `p`: parameters, where p[1] = μ, p[2] = mdot, p[3] = control-law, p[4:6] = τ, θ, β
+    - `t`: time
+"""
+function rhs_cr3bp_control!(du,u,p,t)
+    # unpack parameters
+    μ, mdot, control_law, τ = p[1:4]
+    # get thrust direction
+    tx, ty, tz = control_law(du,u,p,t)
+    # unpack state
+    x, y, z = u[1], u[2], u[3]
+    vx, vy, vz = u[4], u[5], u[6]
+    m = u[7]
+    # compute distances
+    r1 = sqrt( (x+μ)^2 + y^2 + z^2 );
+    r2 = sqrt( (x-1+μ)^2 + y^2 + z^2 );
+    # derivatives of positions
+    du[1] = u[4]
+    du[2] = u[5]
+    du[3] = u[6]
+    # derivatives of velocities
+    du[4] = 2*vy + x - ((1-μ)/r1^3)*(μ+x) + (μ/r2^3)*(1-μ-x) + tx/m
+    du[5] = -2*vx + y - ((1-μ)/r1^3)*y - (μ/r2^3)*y + ty/m
+    du[6] = -((1-μ)/r1^3)*z - (μ/r2^3)*z + tz/m
+    # mass derivative
+    du[7] = -mdot*τ
+end
+
+
+
+"""
+    rhs_bcr4bp_control!(du,u,p,t)
+
+BCR4BP equation of motion with control.
+Expect control-law function in the form:
+
+    ```julia
+        tx, ty, tz = control_law(du,u,p,t)
+    ```
+
+# Arguments
+    - `du`: cache array of duative of state-vector
+    - `u`: state-vector
+    - `p`: parameters, where p[1] = μ, p[2] = μ_3, p[3] = t0, p[4] = a, p[5] = ω_s
+    - `t`: time
+"""
+function rhs_bcr4bp_control!(du,u,p,t)
+    # unpack parameters
+    μ, μ_3, t0, a_s, ω_s, mdot, control_law, τ = p[1:8]
+    # get thrust direction
+    tx, ty, tz = control_law(du,u,p,t)
+
+    # unpack state
+    x, y, z, vx, vy, vz, m = u
+
+    # calculate radii
+    r1 = sqrt( (x+μ)^2 + y^2 + z^2 )
+    r2 = sqrt( (x-1+μ)^2 + y^2 + z^2 )
+
+    # sun position
+    xs = a_s*cos(ω_s*t + t0)
+    ys = a_s*sin(ω_s*t + t0)
+    zs = 0.0
+    r3 = sqrt( (x-xs)^2 + (y-ys)^2 + (z-zs)^2 )
+
+    # position-state derivative
+    du[1] = vx
+    du[2] = vy
+    du[3] = vz
+
+    # velocity derivatives
+    du[4] =  2*vy + x - ((1-μ)/r1^3)*(μ+x) + (μ/r2^3)*(1-μ-x) + ( -(μ_3/r3^3)*(x-xs) - (μ_3/a_s^3)*xs ) + tx/m
+    du[5] = -2*vx + y - ((1-μ)/r1^3)*y      - (μ/r2^3)*y        + ( -(μ_3/r3^3)*(y-ys) - (μ_3/a_s^3)*ys ) + ty/m
+    du[6] =           - ((1-μ)/r1^3)*z      - (μ/r2^3)*z        + ( -(μ_3/r3^3)*(z)    - (μ_3/a_s^3)*zs ) + tz/m
     # mass derivative
     du[7] = -mdot*τ
 end
