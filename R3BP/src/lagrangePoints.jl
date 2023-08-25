55ca60a01e45cb84ef758b3d40e37835d82766be
Yuri <50186716+Yuricst@users.noreply.github.com>
2023-08-25 14:01:47 -0400

    Clarify Bisection() is from the Roots package

--- R3BP/src/lagrangePoints.jl
+++ R3BP/src/lagrangePoints.jl
@@ -12,13 +12,13 @@ function lagrangePoints(μ::Float64)
     l = 1 - μ;
     # collinear points
     fl1(x) = x^5 + 2*(μ-l)*x^4 + (l^2-4*l*μ+μ^2)*x^3 + (2*μ*l*(l-μ)+μ-l)*x^2 + (μ^2*l^2+2*(l^2+μ^2))*x + (μ^3-l^3);
-    xl1 = find_zero(fl1, (0,2), Bisection());
+    xl1 = find_zero(fl1, (0,2), Roots.Bisection());
 
     fl2(x) = x^5 + 2*(μ-l)*x^4 + (l^2-4*l*μ+μ^2)*x^3 + (2*μ*l*(l-μ)-μ-l)*x^2 + (μ^2*l^2+2*(l^2-μ^2))*x - (μ^3+l^3);
-    xl2 = find_zero(fl2, (0,2), Bisection());
+    xl2 = find_zero(fl2, (0,2), Roots.Bisection());
 
     fl3(x) = x^5 + 2*(μ-l)*x^4 + (l^2-4*l*μ+μ^2)*x^3 + (2*μ*l*(l-μ)+μ+l)*x^2 + (μ^2*l^2+2*(μ^2-l^2))*x + (μ^3+l^3);
-    xl3 = find_zero(fl3, (-2,0), Bisection());
+    xl3 = find_zero(fl3, (-2,0), Roots.Bisection());
 
     LP = zeros(5, 6);
     LP[1,1] = xl1;
